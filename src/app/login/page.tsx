f77486c900f207e995796c90a704a2cb8fa20b6d
Eastwood Osei <eastwoodosei0@gmail.com>
2025-05-10 12:35:17 +0000

    modify the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function LoginPage() {
   return(
-    <div>
-      <h1 className="title">Login Page</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-2xl">Log in to you account</legend>
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <button className="btn btn-primary mt-4">Login</button>
+            <div>Don't have an account?<Link href="/" className="link link-hover"><span className="font-bold text-blue-700 ml-2">Sign up now</span></Link></div>
+          </fieldset>
+        </div>
+      </div>
     </div>
+    
   );
 }
