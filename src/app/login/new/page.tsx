c13017b34a7098be4517489a4a63f063a52765ee
Eastwood Osei <eastwoodosei0@gmail.com>
2025-05-10 14:22:08 +0000

    add link for login and  sign up

---
+++ src/app/login/new/page.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+export default function NewLogin() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-2xl">Create your account</legend>
+
+            <label className="label">Full Name</label>
+            <input name="name" type="text" className="input" placeholder="Your Name" />
+
+            <label className="label">Educational Level</label>
+            <select defaultValue="Educational Level" className="select">
+              <option disabled={true}>Select Educational Level</option>
+              <option>SHS 1</option>
+              <option>SHS 2</option>
+              <option>SHS 3</option>
+            </select>
+
+            <label className="label">Program/Course</label>
+            <input name="program" type="text" className="input" placeholder="Program/Course" />
+
+            <label className="label">School Name</label>
+            <input name = "school" type="text" className="input" placeholder="Your School Name" />
+
+            <label className="label">Phone Number</label>
+            <input name = "phone" type="tel" className="input" placeholder="Your Phone Number" />
+
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+
+            <button className="btn btn-primary mt-4">Sign up</button>
+            
+            <div>Already have an account?<Link href="/login" className="link link-hover"><span className="font-bold text-blue-700 ml-2">Log in</span></Link></div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,12 +7,16 @@ export default function LoginPage() {
         <div className="card-body">
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-2xl">Log in to you account</legend>
+
             <label className="label">Email</label>
             <input type="email" className="input" placeholder="Email" />
+
             <label className="label">Password</label>
             <input type="password" className="input" placeholder="Password" />
+
             <button className="btn btn-primary mt-4">Login</button>
-            <div>Don't have an account?<Link href="/" className="link link-hover"><span className="font-bold text-blue-700 ml-2">Sign up now</span></Link></div>
+            
+            <div>Don't have an account?<Link href="/login/new" className="link link-hover"><span className="font-bold text-blue-700 ml-2">Sign up now</span></Link></div>
           </fieldset>
         </div>
       </div>
