e46b47d943c93430746f91d3e923b294a8ae6f3d
Eastwood Osei <eastwoodosei0@gmail.com>
2025-05-16 00:33:14 +0000

    update infocard

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,10 +1,18 @@
 export default function InfoCard({title, info} : {title: string, info: string}) {
   return (
-    <div className="card w-90 bg-white shadow-sm">
-        <div className="card-body">
-          <div className="card-title">{title}</div>
+    <div className="card w-90 bg-white shadow-sm mx-auto">
+        <div className="card-body text-center">
+          <div className="card-title justify-center mx-auto font-bold rounded-full bg-warning w-10 h-10">{getCapitalizedFirstLetter(title)}</div>
+          <div className="card-title justify-center font-bold">{title}</div>
           <p>{info}</p>
         </div>
       </div>
   );
+
+  function getCapitalizedFirstLetter(text: string): string {
+    if(!text){
+      return '';
+    }
+    return text.charAt(0).toUpperCase();
+  }
 }
