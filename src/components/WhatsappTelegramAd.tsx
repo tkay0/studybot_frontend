169454ef9dcff463938c61c0524b275ba202bbe6
Eastwood Osei <eastwoodosei0@gmail.com>
2025-05-20 17:25:34 +0000

    Edit the StudyBot Ad

--- src/components/WhatsappTelegramAd.tsx
+++ src/components/WhatsappTelegramAd.tsx
@@ -3,35 +3,38 @@ import Link from "next/link";
 
 export default function Advert() {
   return (
-    <div className="hero bg-yellow-300 h-85">
+    <div className="hero bg-yellow-300 h-95">
       <div className="hero-content flex-col lg:flex-row">
         <div>
           <h1 className="text-5xl font-bold">Studybot is now on WhatsApp and Telegram</h1>
           <p className="py-6">
             Access your personal study assistant anytime, anywhere. Chat with StudyBot on your favorite platform for quick help, practice questions, and progress tracking.
           </p>
-          <button className="btn btn-outline mr-5">
-            <Link href="/login" className="flex flex-row">
+          <div className="flex flex-col lg:flex-row gap-1">
+            <button className="btn btn-outline w-fit">
+              <Link href="/login" className="flex flex-row">
+                <Image
+                alt="whatsapp logo"
+                src="/whatsapp-icon.svg"
+                width={20}
+                height={20}
+                className="max-w-sm mr-2"
+                />Use StudyBot on WhatsApp
+              </Link>
+            </button>
+            <button className="btn btn-warning bg-white text-black border-0 w-fit">
+              <Link href="/login" className="flex flex-row">
               <Image
-              alt="whatsapp logo"
-              src="/whatsapp-icon.svg"
-              width={20}
-              height={20}
-              className="max-w-sm mr-2"
-              />Use StudyBot on WhatsApp
-            </Link>
-          </button>
-          <button className="btn btn-warning bg-white text-black border-0">
-            <Link href="/login" className="flex flex-row">
-            <Image
-              alt="telegram logo"
-              src="/telegram-icon.svg"
-              width={20}
-              height={20}
-              className="max-w-sm mr-2"
-              />Use StudyBot on Telegram
-            </Link>
-          </button>
+                alt="telegram logo"
+                src="/telegram-icon.svg"
+                width={20}
+                height={20}
+                className="max-w-sm mr-2"
+                />Use StudyBot on Telegram
+              </Link>
+            </button>
+          </div>
+          
         </div>
       </div>
     </div>
